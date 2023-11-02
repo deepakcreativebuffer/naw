ac0efca7403f825195af65b33fbc4be610f7350b
alokcreative <alok@creativebuffer.com>
2023-11-02 19:10:39 +0530

    money type change done

--- components/main/MoneySwitch.js
+++ components/main/MoneySwitch.js
@@ -12,124 +12,116 @@ const pesoIconW = "/static/img/moneyIcons/peso_icon_white.png"
 import Image from 'next/image';
 
 class MoneySwitch extends Component {
-
-    
     render() {
-     
         return (
             <MoneyContext.Consumer>
-                {({ moneyType, setMoneyType }) => {     
-                    // console.log("moneyType>>>",moneyType)    
-                    
-                                  
-                    return(
-
-                    <Fragment>
-                        {this.props.onFooter === true ?
-                            <UncontrolledDropdown className="sty-container-element ">
-                                <DropdownToggle nav>
-                                    <div className='borderboxMoney'>
-                                        <div className="sty-language sty-justify-content ">
-                                            <div className="tag " style={{ marginRight: 7 }}>
-                                                {moneyType === 'USD' ?
-                                                    <div className='icon-moneyF'>
-                                                        <Image src={dolarIconW} width="15px" height="15px" loading='lazy' />
-                                                        <p>{moneyType}</p>
+                {({ moneyType, setMoneyType }) => {
+                    return (
+                        <Fragment>
+                            {this.props.onFooter === true ? (
+                                <UncontrolledDropdown className="sty-container-element">
+                                    <DropdownToggle nav>
+                                        <div className='borderboxMoney'>
+                                            <div className="sty-language sty-justify-content">
+                                                <div className="tag" style={{ marginRight: 7 }}>
+                                                    {moneyType === 'USD' ?
+                                                        <div className='icon-moneyF'>
+                                                            <Image src={dolarIconW} width={15} height={15} loading='lazy' />
+                                                            <p>{moneyType}</p>
+                                                        </div>
+                                                        : moneyType === 'MXN' ?
+                                                            <div className='icon-moneyF'>
+                                                                <Image src={pesoIconW} width={15} height={16} loading='lazy' />
+                                                                <p>{moneyType}</p>
+                                                            </div>
+                                                            :
+                                                            <div className='icon-moneyF'>
+                                                                <Image src={euroIconW} width={13} height={13} loading='lazy' />
+                                                                <p>{moneyType}</p>
+                                                            </div>}
+                                                </div>
+                                                <div>
+                                                    <span className="down"><FontAwesomeIcon icon={faChevronDown} width='16' height='16' /></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </DropdownToggle>
+                                    <DropdownMenu className="drop-footer">
+                                        <DropdownItem
+                                            id="USD"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() => {setMoneyType("USD"); }}>
+                                            <div className="sty-tag"><Image src={dolarIcon} width={15} height={16} style={{ marginBottom: 3 }} loading='lazy' /> USD</div>
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            id="MXN"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() =>{ setMoneyType("MXN");}}>
+                                            <div className="sty-tag"><Image src={pesoIcon} width={15} height={16} style={{ marginBottom: 3 }} /> MXN</div>
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            id="EUR"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() => {setMoneyType("EUR");}}>
+                                            <div className="sty-tag"><Image src={euroIcon} width={15} height={15} style={{ marginBottom: 3 }} /> EUR</div>
+                                        </DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            ) : (
+                                <UncontrolledDropdown className="sty-container-element">
+                                    <DropdownToggle nav>
+                                        <div className="boxMoney sty-language sty-justify-content money-switch">
+                                            <div className="tag">
+                                                {moneyType === 'USD' ? (
+                                                    <div className='icon-money' >
+                                                        <Image src={dolarIconW} width={15} height={16} />
+                                                        <p>{moneyType} </p>
                                                     </div>
-                                                    : moneyType === 'MXN' ?
-                                                        <div className='icon-moneyF'>
-                                                            <Image src={pesoIconW} width="15px" height="16px" loading='lazy'/>
-                                                            <p>{moneyType}</p>
-                                                        </div> :
-                                                        <div className='icon-moneyF'>
-                                                            <Image src={euroIconW} width="13px" height="13px" loading='lazy'/>
-                                                            <p>{moneyType}</p>
-                                                        </div>}
+                                                ) : moneyType === 'MXN' ? (
+                                                    <div className='icon-money'>
+                                                        <Image src={pesoIconW} width={15} height={16} />
+                                                        {moneyType}
+                                                    </div>
+                                                ) : (
+                                                    <div className='icon-money'>
+                                                        <Image src={euroIconW} width={15} height={16} />
+                                                        {moneyType}
+                                                    </div>
+                                                )}
                                             </div>
                                             <div>
-                                                <span className="down"><FontAwesomeIcon icon={faChevronDown} width='16' height='16' /></span>
+                                                <span className="down"><FontAwesomeIcon icon={faChevronDown} className="down-icon" style={{ marginLeft: 25 }} width='16' height='16' /></span>
                                             </div>
                                         </div>
-                                    </div>
-
-                                </DropdownToggle>
-                                <DropdownMenu className="drop-footer">
-                                    <DropdownItem
-                                        id="USD"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("USD")}>
-                                        <div className="sty-tag"><Image src={dolarIcon} width="15px" height="16px" style={{ marginBottom: 3 }} loading='lazy' /> USD</div>
-                                    </DropdownItem>
-                                    {/* <DropdownItem divider /> */}
-                                    <DropdownItem
-                                        id="MXN"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("MXN")}>
-                                        <div className="sty-tag"><Image src={pesoIcon} width="15px" height="16px" style={{ marginBottom: 3 }} />  MXN</div>
-                                    </DropdownItem>
-                                    <DropdownItem
-                                        id="EUR"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("EUR")}>
-                                          <div className="sty-tag" ><Image src={euroIcon} width="15px" height="15px" style={{ marginBottom: 3 }} />  EUR</div>
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                            :
-                            <UncontrolledDropdown className="sty-container-element ">
-                                <DropdownToggle nav >
-                                    <div className="boxMoney sty-language sty-justify-content money-switch">
-                                        <div className="tag">
-                                            {moneyType === 'USD' ?
-                                                <div className='icon-money' >
-                                                    <img src={dolarIconW} width="15px" height="16px" />
-                                                    <p>{moneyType} </p>
-                                                </div>
-                                                : moneyType === 'MXN' ?
-                                                    <div className='icon-money'>
-                                                        <img src={pesoIconW} width="15px" height="16px" />
-                                                        {moneyType}
-                                                    </div> :
-                                                    <div className='icon-money' st>
-                                                        <img src={euroIconW} width="15px" height="16px" />
-                                                        {moneyType}
-                                                    </div>}
-                                        </div>
-                                        <div>
-                                            <span className="down"><FontAwesomeIcon icon={faChevronDown} className="down-icon" style={{ marginLeft: 25 }} width='16' height='16' /></span>
-                                        </div>
-                                    </div>
-                                </DropdownToggle>
-                                <DropdownMenu className="">
-                                    <DropdownItem
-                                        id="USD"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("USD")}>
-                                        <div className="sty-tag"><Image src={dolarIcon} width="15px" height="16px" style={{ marginBottom: 3 }} /> USD</div>
-                                    </DropdownItem>
-                                    {/* <DropdownItem divider /> */}
-                                    <DropdownItem
-                                        id="MXN"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("MXN")}>
-                                        <div className="sty-tag"><Image src={pesoIcon} width="15px" height="16px" style={{ marginBottom: 3 }} />  MXN</div>
-                                    </DropdownItem>
-                                    <DropdownItem
-                                        id="EUR"
-                                        className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
-                                        onClick={() => setMoneyType("EUR")}>
-                                        <div className="sty-tag" ><Image src={euroIcon} width="15px" height="15px" style={{ marginBottom: 3 }} />  EUR</div>
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        }
-                    </Fragment>
-
-                )}}
+                                    </DropdownToggle>
+                                    <DropdownMenu className="">
+                                        <DropdownItem
+                                            id="USD"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() => {setMoneyType("USD");}}>
+                                            <div className="sty-tag"><Image src={dolarIcon} width={15} height={16} style={{ marginBottom: 3 }} /> USD</div>
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            id="MXN"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() => {setMoneyType("MXN");}}>
+                                            <div className="sty-tag"><Image src={pesoIcon} width={15} height={16} style={{ marginBottom: 3 }} /> MXN</div>
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            id="EUR"
+                                            className="sty-uty-font-14-1 sty-justify-content-vertical sty-select-language"
+                                            onClick={() => {setMoneyType("EUR");}}>
+                                            <div className="sty-tag"><Image src={euroIcon} width={15} height={15} style={{ marginBottom: 3 }} /> EUR</div>
+                                        </DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            )}
+                        </Fragment>
+                    );
+                }}
             </MoneyContext.Consumer>
         );
     }
 }
-
 
 export default MoneySwitch;
--- context/moneyContext.js
+++ context/moneyContext.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 const MoneyContext = React.createContext({
@@ -6,4 +5,32 @@ const MoneyContext = React.createContext({
   setMoneyType: () => {}
 });
 
+
+export const setMoneyTypeInContext = async (context) => {
+  try {
+    // Check if the flag is set in local storage
+    const moneytpe = localStorage.getItem("moneyType");
+
+    if (!moneytpe) {
+      const ipResponse = await fetch('http://ip-api.com/json');
+      if (ipResponse.ok) {
+        const userLocation = await ipResponse.json();
+
+        let moneyType;
+        if (userLocation.country === 'Mexico') {
+          moneyType = 'USD';
+        } else if (userLocation.country === 'India') {
+          moneyType = 'EUR';
+        } else if (userLocation.country === 'Spain') {
+          moneyType = 'MXN';
+        }
+
+        context.setMoneyType(moneyType);
+        localStorage.setItem("moneyType",moneyType)
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching user location:", error);
+  }
+};
 export default MoneyContext;
