a422a36240df0873d5de29b5890e0bbc131efbe2
deepakcb <deepak@creativebuffer.com>
2023-11-27 11:45:53 +0530

    set previous enviroment

--- helper/constants/enviroment.js
+++ helper/constants/enviroment.js
@@ -1,21 +1,17 @@
 export default {
   dev: {
-    // 'api_url': 'http://127.0.0.1:8000',
-    api_url: "http://192.168.1.20:8000",
+    api_url: "http://127.0.0.1:8000",
     /*keys*/
-    url: "http://192.168.1.10:3000",
+    url: "http://127.0.0.1",
     googleTag: "GTM-5S9R35R",
     stripe: "pk_test_TYooMQauvdEDq54NiTphI7jx",
     currencyAPI: "3fa28279defc27ba7e9e82a68ed51eb93422baff",
     // 'googleMaps'     : 'AIzaSyBilUDOeh7Vt0FZYweSXzU0lStRAiXJjPk',
   },
   test: {
-    // 'api_url': 'https://stage2.naw.mx:8000',
-    api_url: "http://192.168.1.20:8000", //ankit for backend
-    // api_url: "http://192.168.1.31:8000",
+    api_url: "https://stage2.naw.mx:8000",
     /*keys*/
-    url: "http://192.168.1.10:3000", //sytem ip for frontend
-    // url: "http://192.168.1.31:8000",
+    url: "https://stage2.naw.mx",
     googleTag: "GTM-5S9R35R",
     stripe: "pk_test_FrWxe6SMFv89p7w5Uxcjr5TR",
     currencyAPI: "3fa28279defc27ba7e9e82a68ed51eb93422baff",
